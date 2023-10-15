f98f034eb4c4d642e3f104cd9cddf229b5b5e574
Juli Mostova <julia.mostova89@gmail.com>
2023-10-15 23:01:34 +0300

    change function drop

--- todoOneMoreTime.js
+++ todoOneMoreTime.js
@@ -3,6 +3,7 @@ const input = document.querySelector(".text"),
   contentUl = document.querySelector(".content"),
   createCard = document.querySelector(".createCard");
 
+let draggableTodo = null;
 
 add.addEventListener("click", addTasks);
 
@@ -12,16 +13,17 @@ function addTasks() {
     alert("You neet to type something!");
   } else {
     let li = document.createElement("li");
-    li.innerHTML = `<div class="column"draggable="true">
+    draggableTodo = li;
+    li.innerHTML = `<div class="column" draggable="true" onDragStart(event)>
        ${inpVal} 
-            <button class="close">x</button>
+            <button class="close">х</button>
             </div>`;
+
     contentUl.appendChild(li);
   }
   input.value = "";
- 
+
   delBtn(".close");
-   dragDropShow();
 }
 
 function delBtn(close) {
@@ -59,109 +61,31 @@ function changeTittle() {
 
 changeTittle();
 
-let draggableTodo = null;
-
 function dragDropShow() {
-  let allTodo = document.querySelectorAll(".todo"),
-    colums = document.querySelectorAll(".column");
-console.log(allTodo)
-  
-  for (let i = 0; i < colums.length; i++) {
-    const colum = colums[i];
-console.log(colum)
+  let allTodo = document.querySelectorAll(".todo");
+  console.log(allTodo);
 
-    colum.addEventListener("dragstart", () => {
-      draggableTodo = colum;
-      console.log("dragstart");
+  for (let i = 0; i < allTodo.length; i++) {
+    let todo = allTodo[i];
+
+    console.log(todo);
+
+    todo.addEventListener("dragover", (e) => {
+      e.preventDefault();
+      console.log("dragover");
     });
-
-    colum.addEventListener("dragend", () => {
-      draggableTodo = null;
-      console.log("dragend");
+    todo.addEventListener("dragenter", function (e) {
+      e.preventDefault();
+      console.log("dragenter");
     });
-
-    for (let j = 0; j < allTodo.length; j++) {
-      let todo = allTodo[i];
-
-      console.log(todo)
-
-      todo.addEventListener("dragover", (e) => {
-        e.preventDefault();
-        console.log("dragover");
-      });
-      todo.addEventListener("dragenter", function (e) {
-        e.preventDefault();
-        console.log("dragenter");
-        // this.style.backgroung = "rgba(0,0,0,0.3)";
-      });
-      todo.addEventListener("dragleave", function (e) {
-        // this.style.backgroung = "rgba(0,0,0,.3)";
-        console.log("dragleave");
-      });
-      todo.addEventListener("drop", function (e) {
-        // this.style.backgroung = "rgba(0,0,0,0)";
-        this.append(draggableTodo);
-        console.log("drop");
-      });
-    }
+    todo.addEventListener("dragleave", function (e) {
+      console.log("dragleave");
+    });
+    todo.addEventListener("drop", function (e) {
+      this.append(draggableTodo);
+      console.log("drop");
+    });
   }
-  
-    }
-  
+}
 
 dragDropShow();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // function dragStart(){
-// // draggableTodo = this;
-// // }
-// // function dragEnd(){
-// //   draggableTodo = null;
-// // }
-
-// // allTodo.forEach(item =>{
-// // item.addEventListener('dragover',dragOver);
-// // item.addEventListener('dragenter',dragEnter);
-// // item.addEventListener('dragleave',dragLeave);
-// // item.addEventListener('drop',dragDrop);
-// // });
-
-// // function dragOver(e){
-// // e.preventDefault();
-// // console.log('dragOver')
-// // }
-
-// // function dragEnter(){
-// //  console.log('dragEnter')
-// //   }
-// //   function dragLeave(){
-// //     console.log('dragLeave')
-// //   }
-
-// //   function dragDrop(){
-// //    this.appendChild(draggableTodo);
-// //    console.log('dragDrop')
-// //   }
